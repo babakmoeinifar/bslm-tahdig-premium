8b080187d30493038f0fb510a12394ed6d9770f7
babak <babakus7@gmail.com>
2023-06-21 12:01:26 +0330

    wip

--- src/database/migrations/2020_10_25_131846_rename_tables.php
+++ src/database/migrations/2020_10_25_131846_rename_tables.php
@@ -9,14 +9,12 @@ class RenameTables extends Migration
     public function up()
     {
         Schema::rename('bookings', 'tahdig_bookings');
-        Schema::rename('booking_food', 'food_tahdig_booking');
         Schema::rename('reservations', 'tahdig_reservations');
     }
 
     public function down()
     {
         Schema::rename('tahdig_bookings', 'bookings');
-        Schema::rename('food_tahdig_booking', 'booking_food');
         Schema::rename('tahdig_reservations', 'reservations');
     }
 }
